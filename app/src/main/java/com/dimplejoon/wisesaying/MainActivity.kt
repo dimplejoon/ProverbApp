440b9cc1ce89b0dbc5a613226a05642b9c63fb5a
dimplejoon <76550158+dimplejoon@users.noreply.github.com>
2023-08-05 20:17:47 +0900

    to study Databinding

--- app/src/main/java/com/dimplejoon/wisesaying/MainActivity.kt
+++ app/src/main/java/com/dimplejoon/wisesaying/MainActivity.kt
@@ -3,6 +3,7 @@ package com.dimplejoon.wisesaying
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import com.dimplejoon.wisesaying.databinding.ActivityMainBinding
 
@@ -15,6 +16,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val sentenceList = mutableListOf<String>()
+        sentenceList.add("住めば都")
+        sentenceList.add("頭は隠して尻隠さず")
+        sentenceList.add("後の祭り")
+        sentenceList.add("石の上でも三年")
+        sentenceList.add("石橋を叩いて渡る")
+        sentenceList.add("馬の耳に念仏")
+        sentenceList.add("鰯の頭も信心から")
+        sentenceList.add("縁の下の力持ち")
+        sentenceList.add("同じ釜の飯を食う")
+        sentenceList.add("河童の川流れ")
+        sentenceList.add("清水の舞台から飛び降りる")
+        sentenceList.add("知らぬが仏")
+        sentenceList.add("捕らぬ狸の皮算用")
+        sentenceList.add("三人寄れば文殊の知恵")
+
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.showAllSentenceBtn.setOnClickListener{
 
@@ -22,6 +39,6 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
 
         }
-
+        binding.wiseSayingArea.setText(sentenceList.random())
     }
 }
--- app/src/main/java/com/dimplejoon/wisesaying/SentenceActivity.kt
+++ app/src/main/java/com/dimplejoon/wisesaying/SentenceActivity.kt
@@ -2,6 +2,7 @@ package com.dimplejoon.wisesaying
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
 
 class SentenceActivity : AppCompatActivity() {
 
@@ -9,6 +10,28 @@ class SentenceActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sentence)
+
+        val sentenceList = mutableListOf<String>()
+
+        sentenceList.add("住めば都")
+        sentenceList.add("頭は隠して尻隠さず")
+        sentenceList.add("後の祭り")
+        sentenceList.add("石の上でも三年")
+        sentenceList.add("石橋を叩いて渡る")
+        sentenceList.add("馬の耳に念仏")
+        sentenceList.add("鰯の頭も信心から")
+        sentenceList.add("縁の下の力持ち")
+        sentenceList.add("同じ釜の飯を食う")
+        sentenceList.add("河童の川流れ")
+        sentenceList.add("清水の舞台から飛び降りる")
+        sentenceList.add("知らぬが仏")
+        sentenceList.add("捕らぬ狸の皮算用")
+        sentenceList.add("三人寄れば文殊の知恵")
+
+        val sentenceAdapter = ListViewAdapter(sentenceList)
+        val listview = findViewById<ListView>(R.id.sentenceListView)
+
+        listview.adapter = sentenceAdapter
 
     }
 }
